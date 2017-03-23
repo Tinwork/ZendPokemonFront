50096ec901153b58211707b7dcb8505103716bc4
Antoine Renault <antoine.renault.mmi@gmail.com>
2017-03-23 10:08:26 +0100

    Init with create-react-app

--- app/hello.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
- 
-ReactDOM.render(
-  <h1>Hello, world zzz! ta maman</h1>,
-  document.getElementById('root')
-);
