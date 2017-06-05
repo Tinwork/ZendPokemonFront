36d1d0cb9bfbe1f2deae60f2bf2595043bd97422
Antoine Renault <antoine.renault.mmi@gmail.com>
2017-06-05 22:28:56 +0200

    Add WIP Store

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,78 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    count: 0
+const state = {
+  pokemonShow: {},
+  pokemonMap: {},
+  pokemons: []
+}
+
+const mutations = {
+  setPokemonShowWithId: function (state, id) {
+    let i
+    let tmp = state.pokemons.filter((pokemon, index) => {
+      if (pokemon.id === id) {
+        i = index
+        return pokemon
+      }
+    })
+    tmp.index = i
+    state.pokemonShow = tmp
   },
-  mutations: {
-    increment (state) {
-      state.count++
+  setPokemonshowWithIndex: function (state, index) {
+    state.pokemonShow = state.pokemons[index]
+    state.pokemonShow.index = index
+  },
+  setPokemonMap: function (state, pokemon) {
+    state.pokemonMap = pokemon
+  },
+  setPokemons: function (state, array) {
+    if (array.lenght !== 0) {
+      state.pokemons = array
     }
+  },
+  filterById: function (state, comparaison) {
+    let tmp
+    switch (comparaison) {
+      case 'more':
+        tmp = state.pokemons.sort((a, b) => {
+          return b.nbVotes - a.nbVotes
+        })
+        break
+      case 'less':
+        tmp = state.pokemons.sort((a, b) => {
+          return a.nbVotes - b.nbVotes
+        })
+        break
+    }
+    state.pokemons = tmp
+  },
+  filterByName: function (state, comparaison) {
+    let tmp
+    switch (comparaison) {
+      case 'asc':
+        tmp = state.pokemons.sort((a, b) => {
+          return b.title.localeCompare(a.title)
+        })
+        break
+      case 'desc':
+        tmp = state.pokemons.sort((a, b) => {
+          return a.title.localeCompare(b.title)
+        })
+        break
+    }
+    state.pokemons = tmp
   }
+}
+
+const getters = {
+  pokemonMap: state => {
+    return state.pokemonMap
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters
 })
