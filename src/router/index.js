99598d1ef07c4aeca0d0c56f582068f81ab9a761
Antoine Renault <antoine.renault.mmi@gmail.com>
2017-06-05 15:14:19 +0200

    Add complete functionnal router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Map from '@/components/Map'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/map',
+      name: 'Map',
+      component: Map
+    },
+    {
+      path: '*',
+      redirect: {
+        name: 'Home'
+      }
     }
   ]
 })
